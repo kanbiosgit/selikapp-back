40425d1e81b52199e7103c7918a011e19a7a4025
victor <victor.maurin@epitech.eu>
2021-04-14 10:00:40 +0200

    [FIX]: refacto user login, logic is now in the post function not in serializer

--- selika_api/user/serializer.py
+++ selika_api/user/serializer.py
@@ -1,51 +1,10 @@
 from os import access
 from rest_framework import serializers
-from .models import AAUser
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import update_last_login
-from rest_framework_simplejwt.tokens import RefreshToken
+
 from userprofile.models import UserProfile, UserCustomGroup
 
 
 
 class UserLoginSerializer(serializers.Serializer):
     email = serializers.CharField(max_length=255)
-    password = serializers.CharField(max_length=128, write_only=True)
-    access = serializers.CharField(max_length=255, read_only=True)
-    refresh = serializers.CharField(max_length=255, read_only=True)
-
-    def validate(self, data):
-        print('data', data)
-        email = data.get("email", None)
-        password = data.get("password", None)
-        user = authenticate(email=email, password=password)
-        if user is None:
-          raise serializers.ValidationError(
-              'A user with this email and password is not found.'
-          )
-        # profile_data = data.get("profile", None)
-        # print('profile_data', profile_data)
-        #
-        # custom_group = UserCustomGroup.objects.create(
-        #   label = profile_data['custom_group']['label']
-        # )
-        # if profile_data is not None :
-        #     if UserProfile(user=user) is None :
-        #       UserProfile.objects.create(
-        #         user = user,
-        #         firstname = profile_data['first_name'],
-        #         lastname = profile_data['last_name'],
-        #         custom_group = custom_group,
-        #       )
-        try:
-            refresh = RefreshToken.for_user(user)
-            update_last_login(None, user)
-        except AAUser.DoesNotExist:
-            raise serializers.ValidationError(
-                'User with given email and password does not exists'
-            )
-        return {
-            'email': email,
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        }
+    password = serializers.CharField(max_length=128)
--- selika_api/user/views.py
+++ selika_api/user/views.py
@@ -3,9 +3,14 @@ from user.models import AAUser
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from .serializer import  UserLoginSerializer
+from .serializer import UserLoginSerializer
 from rest_framework.generics import RetrieveAPIView
+from django.contrib.auth import authenticate
+from .models import AAUser
+from django.contrib.auth.models import update_last_login
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
+
 
 class UserLoginView(RetrieveAPIView):
     permission_classes = (AllowAny,)
@@ -15,18 +20,33 @@ class UserLoginView(RetrieveAPIView):
         print('request data', request.data)
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        response = {
-            'success' : 'True',
-            'status code' : status.HTTP_200_OK,
-            'message': 'User logged in  successfully',
-            'access_token' : serializer.data['access'],
-            'refresh_token' : serializer.data['refresh'],
+        email = serializer.data['email']
+        password = serializer.data['password']
+        user = authenticate(email=email, password=password)
+        if user is None:
+            response = {
+                'success': False,
+                'status code': status.HTTP_400_BAD_REQUEST,
+                'message': 'user not found'
+            }
+        else:
+            try:
+                refresh = RefreshToken.for_user(user)
+                update_last_login(None, user)
+            except AAUser.DoesNotExist:
+                response = {
+                    'success': False,
+                    'status code': status.HTTP_400_BAD_REQUEST,
+                    'message': 'User with given email and password does not exists'
+                }
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            response = {
+                'success': 'True',
+                'status code': status.HTTP_200_OK,
+                'message': 'User logged in  successfully',
+                'access_token': str(refresh.access_token),
+                'refresh_token': str(refresh),
             }
         status_code = status.HTTP_200_OK
 
         return Response(response, status=status_code)
-
-
-
-
-
