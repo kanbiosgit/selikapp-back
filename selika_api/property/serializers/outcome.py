749ea19d5fcaabf06b86743664580f96f995bd35
victor <victor.maurin@epitech.eu>
2021-05-03 16:19:32 +0200

    [ADD]: negociator from property

--- selika_api/property/serializers/outcome.py
+++ selika_api/property/serializers/outcome.py
@@ -1,8 +1,12 @@
 from property.models import Property
 from rest_framework import serializers
+from prospecting.serializers.outcome import NegociatorOutcomeSerializer
+
 
 
 class PropertyOutcomeSerializer(serializers.ModelSerializer):
+    negociator = NegociatorOutcomeSerializer(required=False)
+
     class Meta:
       model = Property
-      fields = ['name', 'phone', 'address', 'price', 'ground', 'comment', 'ref', 'prospecting', 'id', 'creation']
+      fields = ['name', 'phone', 'address', 'price', 'ground', 'comment', 'ref', 'prospecting', 'id', 'creation', 'negociator']
